e0acf5441f91f1e575d1871f56f565de6c51fda7
Vinh Bui <vinhbui.tg@gmail.com>
2022-07-25 22:31:43 +0700

    update explain

--- leetcode/algorithms/206_reverse_linked_list.py
+++ leetcode/algorithms/206_reverse_linked_list.py
@@ -7,11 +7,25 @@ class ListNode:
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev_node = None
+        curr_node = head
 
-        while head:
-            next_node = head.next
-            head.next = prev_node
-            prev_node = head
-            head = next_node
+        while curr_node:
+            next_node = curr_node.next
+            curr_node.next = prev_node
+            prev_node = curr_node
+            curr_node = next_node
 
         return prev_node
+
+
+# a -> b -> c -> d -> null
+
+# prev_node = null
+# curr_node = a
+# next_node = b
+
+# first loop
+# null <- a <- b
+# prev_node.next = null
+# prev_node = a
+# curr_node = b
