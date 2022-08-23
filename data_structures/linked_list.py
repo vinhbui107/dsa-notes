07957fc0a4c6f9e36161d68acd3e48f4a07d705e
Vinh Bui <vinhbui.tg@gmail.com>
2022-08-23 20:46:58 +0700

    create ADT of linked list

---
+++ data_structures/linked_list.py
@@ -0,0 +1,58 @@
+# SINGLE LINKEDLIST
+class Node:
+    def __init__(self, next_node=None, previous_node=None, value=None):
+        next_node = next_node
+        previous_node = previous_node
+        value = value
+
+
+class SingleLinkedList:
+    def __init__(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def size(self):
+        pass
+
+    def insert(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def find(self):
+        pass
+
+    def remove_at(self):
+        pass
+
+    def replace(self):
+        pass
+
+
+class DoubleLinkedList:
+    def __init__(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def size(self):
+        pass
+
+    def insert(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def find(self):
+        pass
+
+    def remove_at(self):
+        pass
+
+    def replace(self):
+        pass
