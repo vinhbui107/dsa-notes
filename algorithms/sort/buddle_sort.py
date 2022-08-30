43e5733352c085dccf39e3ad9551b90772e881f7
Vinh Bui <vinhbui.tg@gmail.com>
2022-08-30 22:47:09 +0700

    Change param name to collection

--- algorithms/sort/buddle_sort.py
+++ algorithms/sort/buddle_sort.py
@@ -1,47 +1,47 @@
-def bubble_sort(items: list[int]) -> list[int]:
-    if not items:
+def bubble_sort(collection: list[int]) -> list[int]:
+    if not collection:
         return []
 
     loop_count = 0
-    for i in range(len(items)):
-        for j in range(0, len(items) - i - 1):
+    for i in range(len(collection)):
+        for j in range(0, len(collection) - i - 1):
             loop_count += 1
 
-            if items[j] > items[j + 1]:
-                items[j], items[j + 1] = items[j + 1], items[j]
+            if collection[j] > collection[j + 1]:
+                collection[j], collection[j + 1] = collection[j + 1], collection[j]
 
     print(f"Number of loop: {loop_count}")
-    return items
+    return collection
 
 
-a = [10, 24, 32, 21, 1, 8, 9, 345]
-print(f"List a: {a}")
-print(f"List a after sort: {bubble_sort(a)}")
+items = [10, 24, 32, 21, 1, 8, 9, 345]
+print(f"List a: {items}")
+print(f"List a after sort: {bubble_sort(items)}")
 
 
-def bubble_sort_optimized(items: list[int]) -> list[int]:
-    if not items:
+def bubble_sort_optimized(collection: list[int]) -> list[int]:
+    if not collection:
         return []
 
     loop_count = 0
-    for i in range(len(items)):
+    for i in range(len(collection)):
         # keep track of swapping
         swapped = False
 
-        for j in range(0, len(items) - i - 1):
+        for j in range(0, len(collection) - i - 1):
             loop_count += 1
 
-            if items[j] > items[j + 1]:
-                items[j], items[j + 1] = items[j + 1], items[j]
+            if collection[j] > collection[j + 1]:
+                collection[j], collection[j + 1] = collection[j + 1], collection[j]
                 swapped = True
 
         if not swapped:
             break
 
     print(f"Number of loop: {loop_count}")
-    return items
+    return collection
 
 
-a = [24, 14, 32, 21, 1, 8, 9, 345]
-print(f"\nList a: {a}")
-print(f"List a after sort: {bubble_sort_optimized(a)}")
+items = [24, 14, 32, 21, 1, 8, 9, 345]
+print(f"\nList a: {items}")
+print(f"List a after sort: {bubble_sort_optimized(items)}")
--- algorithms/sort/intersection_sort.py
+++ algorithms/sort/intersection_sort.py
@@ -1,18 +1,20 @@
-def intersection_sort(items: list[int]) -> list[int]:
-    if not items:
+def intersection_sort(collection: list[int]) -> list[int]:
+    if not collection:
         return []
 
-    items_sorted = []
-    for item in items:
-        for item_sorted in items_sorted:
-            if item_sorted > item:
-                pass
-            elif item_sorted < item:
-                pass
-            else:
-                pass
+    for i in range(1, len(collection)):
+        current_item = collection[i]
+
+        j = i - 1
+        while j >= 0 and collection[j] > current_item:
+            collection[j + 1] = collection[j]
+            j -= 1
+
+        collection[j + 1] = current_item
+
+    return collection
 
 
-a = [10, 24, 32, 21, 1, 8, 9, 345]
-print(f"List a: {a}")
-print(f"List a after sort: {intersection_sort(a)}")
+items = [10, 24, 32, 21, 1, 8, 9, 345]
+print(f"List a: {items}")
+print(f"List a after sort: {intersection_sort(items)}")
