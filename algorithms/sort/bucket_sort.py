ab887f740b0726821f442a34a576428d6220a2f5
vinhbui107 <vinhbui.tg@gmail.com>
2024-01-08 13:38:37 +0700

    add bucket sort

---
+++ algorithms/sort/bucket_sort.py
@@ -0,0 +1,23 @@
+def bucket_sort(nums: list[int], bucket_size: int = 5) -> list[int]:
+    """
+    >>> bucket_sort(nums=[3, 4, 5, 2, 1, 100, 7])
+    [1, 2, 3, 4, 5, 7, 100]
+    """
+    max_val, min_val = max(nums), min(nums)
+    span = (max_val - min_val + 1) // bucket_size
+    buckets = [[] for _ in range(bucket_size)]
+
+    for num in nums:
+        index = (num - min_val) // span
+
+        if index == bucket_size:
+            index -= 1
+
+        buckets[index].append(num)
+
+    sorted_arr = []
+    for bucket in buckets:
+        sorted_arr.extend(sorted(bucket))
+
+    return sorted_arr
+
