b5907afed6e7760b433944701480712b1bb0d648
vinhbui107 <vinhbui.tg@gmail.com>
2024-01-09 10:54:44 +0700

    add doctest

--- algorithms/search/binary_search.py
+++ algorithms/search/binary_search.py
@@ -1,13 +1,23 @@
-def binary_search(collection: list[int], key: int) -> bool:
-    if not collection:
+def binary_search(nums: list[int], key: int) -> bool:
+    """
+    Examples:
+    >>> binary_search([1, 2, 3, 4, 5, 6, 8], 1)
+    True
+    >>> binary_search([1, 2, 3, 4, 5, 6, 8], 10)
+    False
+    >>> binary_search([])
+    False
+    """
+    if not nums:
         return False
 
     left = 0
-    right = len(collection) - 1
+    right = len(nums) - 1
 
     while left < right:
         mid_point = left + (right - left)
-        current_item = collection[mid_point]
+        current_item = nums[mid_point]
+
         if current_item == key:
             return True
         elif current_item < key:
@@ -18,11 +28,6 @@ def binary_search(collection: list[int], key: int) -> bool:
     return False
 
 
-key_search = 2
-items_a = [1, 2, 3, 4, 5, 6, 8]
-print(f"List a: {items_a}")
-print(f"Is key {key_search} in items: {binary_search(collection=items_a, key=key_search)}")
-
-key_search = 10
-print(f"List a: {items_a}")
-print(f"Is key {key_search} in items: {binary_search(collection=items_a, key=key_search)}")
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
