597cc5aa5f61783c1b60198693602620e2f46328
vinhbui <vinhbui.tg@gmail.com>
2024-03-18 21:50:28 +0700

    add target sum solution

--- algorithms/techniques/two_pointers.py
+++ algorithms/techniques/two_pointers.py
@@ -21,6 +21,34 @@ def is_palindrome_array(nums: list[int]) -> bool:
     return True
 
 
+def target_sum(nums: list[int], target: int) -> [int, int]:
+    """
+    Give sorted array nums and target, find the sum of 2 elements (diff position) that sum to the target
+
+    >>> target_sum([1, 5, 2, 7, 2], 7)
+    [1, 2]
+
+    >>> target_sum([1, 5, 2, 7, 2], 10)
+    []
+
+    >>> target_sum([1, 5, 2, 7, 3], 10)
+    [3, 4]
+    """
+    if len(nums) < 2:
+        return []
+
+    L, R = 0, 1
+
+    while R < len(nums):
+        if nums[L] + nums[R] == target:
+            return [L, R]
+        else:
+            L += 1
+            R += 1
+
+    return []
+
+
 if __name__ == "__main__":
     import doctest
 
