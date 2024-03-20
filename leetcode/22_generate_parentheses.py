e244a41080dbee054f2836277f7d2ec3296ce276
vinhbui107 <vinhbui.tg@gmail.com>
2024-03-20 21:58:19 +0700

    add generate parentheses solution

---
+++ leetcode/22_generate_parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    """
+    https://leetcode.com/problems/two-sum/
+
+    >>> solution = Solution()
+    >>> solution.generateParenthesis(3)
+    ["((()))","(()())","(())()","()(())","()()()"]
+    """
+    def generateParenthesis(self, n: int) -> list[str]:
+        return []
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
